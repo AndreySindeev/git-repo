69b3a005ffab9079f9b9102954dd9257582f4981
Andrey <asindeev@open-sys.org>
2017-10-27 16:36:48 +0300

    Create DELTA.py

---
+++ DELTA.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+def get_delta():
+    results1 = int(last_release.get())
+    results2 = int(new_release.get())
+    delta = results2 / results1
+    print(delta)
+
+windows = Tk()
+windows.geometry('500x500')
+#создаём главное окно
+t1 = Label(windows, text='Enter your values', fg='green')
+#задаём стиль окна
+t1.config(font=('Verdana', 25))
+t1.pack()
+#задаём поля ввода для прошлого релиза
+
+last_release = Entry(windows, width = 30, bg='yellow')
+last_release.focus()
+last_release.pack()
+
+#задаём поле ввода для нового релиза
+new_release = Entry(windows, width = 30, bg='red')
+new_release.pack()
+
+
+#создаём кнопку ,которая выводит нам результат
+but = Button(windows, text = 'get the percentage difference', command= get_delta)
+but.pack()
+windows.mainloop()
