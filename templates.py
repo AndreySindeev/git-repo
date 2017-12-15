8c082f565f6b3580404a424e5e121dbbd1ee97b5
Andrey <asindeev@open-sys.org>
2017-12-15 13:51:03 +0300

    Update templates.py

--- templates.py
+++ templates.py
@@ -20,3 +20,27 @@ def task3(distances):
 result = task3((
         ('Oslo', 'Chita', 234),
         ('London', 'Astana', 743),))
+"""
+def task(seq):
+    result = []
+    for each in seq:
+        if isinstance(each, (list, set, tuple)):
+            result.extend(each)
+        else:
+            result.append(each)
+    result.sort()
+
+    print(result)
+    return result
+def task2(seq):
+    result = {}
+    for val in seq:
+        if val not in result:
+            result[val] = 1
+        else:
+            result[val] +=1
+    print (result)
+    return result
+
+task2([10, 20, 20, 21, 30, 21, 10])
+"""
